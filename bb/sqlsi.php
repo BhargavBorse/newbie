2e728a54b627814a694d20f9e80394e7df4d368c
Bhargav <bhargavborse4898@gmail.com>
2019-10-11 13:10:31 +0530

    imp session in comment

--- bb/sqlsi.php
+++ bb/sqlsi.php
@@ -18,3 +18,48 @@ $db=mysqli_connect('localhost','root','','master_nbh') or die("Error 404 not fou
       }
     }
  ?>
+
+
+//         {
+//           if(empty($Email))
+// {
+//   $errors[] = "you forgot the email";
+// }
+//   if (empty($Password))
+// {
+//   $errors[] = "you forgot the password";
+//   }
+//
+// echo "<br> no error in entering data.. <br> proceeding with query";
+//
+//
+//   $retrieve = "SELECT UserName FROM signup where Email = 'Email' AND Password = SHA1('Password')";
+//
+//   $run = mysqli_query($db,$retrieve) or die("error in query");
+//
+// if (mysqli_num_rows($run) == 1 )
+// {
+//  echo "<br> Query run succesfully";
+//   $row = mysqli_fetch_array($run, MYSQLI_ASSOC);
+//
+// return array(true,$row);
+//
+// $q1= "SELECT UserName FROM signup where Email = '$Email' AND Password = SHA1('$Password') ";
+// $q2= "SELECT Password FROM signup where Email = '$Email' AND Password = SHA1('$Password')";
+// $Email = @mysqli_query($db, $q1);
+// $Password = @mysqli_query($db, $q2);
+//
+//
+//
+//
+//   setcookie("Email",$Email);
+//   header("location : mainhome.php");
+//   exit();
+//
+//
+// }
+// else {
+//   echo "<br> Email and password do not match";
+// }
+
+        //}
